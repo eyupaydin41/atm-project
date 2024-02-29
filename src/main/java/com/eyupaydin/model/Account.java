f3b45b99ba00b915d6be97a6946b3686f032d2b7
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-02-29 12:41:37 +0300

    Create Account Entity

---
+++ src/main/java/com/eyupaydin/model/Account.java
@@ -0,0 +1,25 @@
+package com.eyupaydin.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_number", nullable = false, unique = true)
+    private String accountNumber;
+
+    @Column(name = "balance")
+    private double balance;
+
+}
