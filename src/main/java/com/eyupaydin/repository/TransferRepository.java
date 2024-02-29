6a8b63484cc576e80e29c84f1f71e381fd4d1d0b
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-02-29 23:36:16 +0300

    created TransferRepository

---
+++ src/main/java/com/eyupaydin/repository/TransferRepository.java
@@ -0,0 +1,9 @@
+package com.eyupaydin.repository;
+
+import com.eyupaydin.model.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+}
