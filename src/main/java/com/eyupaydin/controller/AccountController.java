6bc5e74790fcd00f5f53a02531d3ef2412842690
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-02-29 23:32:51 +0300

    Updated endpoint URLs

--- src/main/java/com/eyupaydin/controller/AccountController.java
+++ src/main/java/com/eyupaydin/controller/AccountController.java
@@ -1,20 +1,15 @@
 package com.eyupaydin.controller;
 
 import com.eyupaydin.dto.AccountDto;
-import com.eyupaydin.model.Account;
 import com.eyupaydin.service.AccountServices;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     public final AccountServices accountService;
@@ -24,28 +19,32 @@ public class AccountController {
     }
 
 
-    //http://localhost:8080/api/accounts
-    @GetMapping("/accounts")
+    // http://localhost:8080/api/accounts
+    @GetMapping
     public List<AccountDto> getAllAccounts() {
         return accountService.getAllAccounts();
     }
 
-    @PostMapping("/accounts")
+    // http://localhost:8080/api/accounts
+    @PostMapping
     public AccountDto createAccount(@RequestBody AccountDto accountDto) {
         return accountService.createAccount(accountDto);
     }
 
-    @GetMapping("/accounts/{id}")
+    // http://localhost:8080/api/accounts/1
+    @GetMapping("/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
         return accountService.getAccountById(id);
     }
 
-    @PutMapping("/accounts/{id}")
+    // http://localhost:8080/api/accounts/1
+    @PutMapping("/{id}")
     public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id, @RequestBody AccountDto accountDto) {
         return accountService.updateAccount(id, accountDto);
     }
 
-    @DeleteMapping("/accounts/{id}")
+    // http://localhost:8080/api/accounts/1
+    @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteAccount(@PathVariable Long id) {
         return accountService.deleteAccount(id);
     }
