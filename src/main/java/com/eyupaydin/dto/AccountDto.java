d09572d1207d45af12948d9f457c829f7b437d1e
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-02-29 12:41:17 +0300

    Create AccountDto

---
+++ src/main/java/com/eyupaydin/dto/AccountDto.java
@@ -0,0 +1,16 @@
+package com.eyupaydin.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDto {
+
+    private String accountNumber;
+    private double balance;
+
+}
